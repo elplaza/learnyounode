bcc0dddea8bc89c96ef17f0ad11473e78e779619
Federico Piazzon <federico.piazzon@gmail.com>
2019-03-19 10:15:55 +0100

    my first nodejs server

--- time-server.js
+++ time-server.js
@@ -2,22 +2,35 @@ var net = require('net');
 
 var port = process.argv[2];
 
-var server = net.createServer((socket) => {
+function nowString () {
     var date = new Date();
 
-    var dateStr = date.getFullYear()
-        + '-'
-        + date.getMonth()
-        + '-'
-        + date.getDate()
-        + ' '
-        + date.getHours()
-        + ':'
-        + date.getMinutes()
-    ;
+    var year = String(date.getFullYear());
+    var month = String(date.getMonth() + 1);
+    var day = String(date.getDate());
+    var hour = String(date.getHours());
+    var minutes = String(date.getMinutes());
 
-    socket.write(dateStr);
+    return year.padStart(4, '0')
+        + '-'
+        + month.padStart(2, '0')
+        + '-'
+        + day.padStart(2, '0')
+        + ' '
+        + hour.padStart(2, '0')
+        + ':'
+        + minutes.padStart(2, '0')
+        + '\n'
+    ;
+}
+
+var server = net.createServer((socket) => {
+    socket.write(nowString());
     socket.end();
+});
+
+server.on('error', (err) => {
+    console.error(err);
 });
 
 server.listen(port);
